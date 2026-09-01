196417b3389878df5c58f137394bea3d3e7cb90d
abenezer <122831522+abenilove@users.noreply.github.com>
2026-09-01 16:36:40 +0300

    Add Gradle wrapper files

--- MezmureTewahedo/gradlew.bat
+++ MezmureTewahedo/gradlew.bat
@@ -14,7 +14,7 @@
 @rem limitations under the License.
 @rem
 
-@if "%DEBUG%"=="" @echo off
+@if "%DEBUG%"==\"\" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
@@ -22,18 +22,18 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+if "%OS%"==\"Windows_NT\" setlocal
 
 set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
+if "%DIRNAME%"==\"\" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+@rem Resolve any \".\" and \"..\" in APP_HOME to make it shorter.
+for %%i in (\"%APP_HOME%\") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=\"-Xmx64m\" \"-Xms64m\"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -51,10 +51,10 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=%JAVA_HOME:\"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto execute
+if exist \"%JAVA_EXE%\" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -71,18 +71,18 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+\"%JAVA_EXE%\" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% \"-Dorg.gradle.appname=%APP_BASE_NAME%\" -classpath \"%CLASSPATH%\" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
 @rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
+if \"%ERRORLEVEL%\"==\"0\" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE to error code if problem was found
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit %GRADLE_EXIT_CONSOLE%
+if  not \"\" == \"%GRADLE_EXIT_CONSOLE%\" exit %GRADLE_EXIT_CONSOLE%
 exit /b 1
 
 :mainEnd
-if "%OS%"=="Windows_NT" endlocal
+if \"%OS%\"==\"Windows_NT\" endlocal
 
 :omega
